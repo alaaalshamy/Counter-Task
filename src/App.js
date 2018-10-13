24382592398d8fc81b46251df5e55044cac08012
Alaa <alaaalshamy44@yahoo.com>
2018-10-14 00:09:10 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import BigCounters from "./components/bigCounters";
+import Navbar from "./components/navbar";
 
 class App extends Component {
+  state = {
+    counters: [
+      {
+        id: 0,
+        value: 4
+      },
+      {
+        id: 1,
+        value: 0
+      },
+      {
+        id: 2,
+        value: 0
+      },
+      {
+        id: 3,
+        value: 0
+      }
+    ]
+  };
+
+  handelIncrement = counter => {
+    const newCounter = [...this.state.counters];
+    newCounter[counter.id].value++;
+
+    this.setState({ counters: newCounter });
+  };
+  handelReset = () => {
+    const newCounterValue = this.state.counters.map(counter => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters: newCounterValue });
+  };
+
+  handelDelete = counterID => {
+    const newCounter = this.state.counters.filter(c => c.id !== counterID);
+    this.setState({ counters: newCounter });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <main className="container">
+          <BigCounters
+            counters={this.state.counters}
+            onReset={this.handelReset}
+            onDelete={this.handelDelete}
+            onIncrement={this.handelIncrement}
+          />
+        </main>
+      </React.Fragment>
     );
   }
 }
--- src/components/bigCounters.jsx
+++ src/components/bigCounters.jsx
@@ -1,57 +1,21 @@
 import React, { Component } from "react";
 import Counter from "./counter";
 class BigCounters extends Component {
-  state = {
-    counters: [
-      {
-        id: 0,
-        value: 4
-      },
-      {
-        id: 1,
-        value: 0
-      },
-      {
-        id: 2,
-        value: 0
-      },
-      {
-        id: 3,
-        value: 0
-      }
-    ]
-  };
-
-  handelIncrement = counter => {
-    const newCounter = [...this.state.counters];
-    newCounter[counter.id].value++;
-
-    this.setState({ counters: newCounter });
-  };
-  handelReset = () => {
-    const newCounterValue = this.state.counters.map(counter => {
-      counter.value = 0;
-      return counter;
-    });
-    this.setState({ counters: newCounterValue });
-  };
-
-  handelDelete = counterID => {
-    const newCounter = this.state.counters.filter(c => c.id !== counterID);
-    this.setState({ counters: newCounter });
-  };
   render() {
     return (
       <div>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.handelDelete}
-            onIncrement={this.handelIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
           />
         ))}
-        <button className="btn btn-info btm-sm sm-2" onClick={this.handelReset}>
+        <button
+          className="btn btn-info btm-sm sm-2"
+          onClick={this.props.onReset}
+        >
           Reset
         </button>
       </div>
